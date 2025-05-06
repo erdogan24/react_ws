560738486d994686a312dab2fe54e1fb76b96a91
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-05-07 00:43:22 +0100

    commit 2

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,14 @@
 export function SignUp() {
+  const onChangeUsername = (event) => {
+    console.log(event);
+  };
   return (
     <>
       <h1>Sign Up</h1>
 
       <div>
         <label htmlFor="username">Username</label>
-        <input id="username" />
+        <input id="username" onChange={onChangeUsername} />
       </div>
 
       <div>
@@ -17,6 +20,13 @@ export function SignUp() {
         <label htmlFor="password">Password</label>
         <input id="password" type="password" />
       </div>
+
+      <div>
+        <label htmlFor="passwordRepeat">Password Repeat</label>
+        <input id="passwordRepeat" type="password" />
+      </div>
+
+      <button>Sign Up</button>
     </>
   );
 }
